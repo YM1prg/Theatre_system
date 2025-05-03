5b0df2419ccef91131fcb7b8b2d0ff72b04a3038
Mark Hany <markhanyadel05@gmail.com>
2025-05-03 15:55:46 +0300

    backend + some comandline to explain it

--- Theatre.java
+++ Theatre.java
@@ -3,52 +3,50 @@ package Theatre_system;
 import java.util.Scanner;
 public class theatre {
     Scanner input = new Scanner(System.in);
-    private final static int FirstClassCost=50;
-    private final static int SecondClassCost=15;
-    private final static int ThirdClassCost=10;
+    private final static int FirstClassCost=50;//
+    private final static int SecondClassCost=15;//     The fixed cost for each class
+    private final static int ThirdClassCost=10;//
 
-    private final static int FirstClassSeats=20;
-    private final static int SecondClassSeats=40;
-    private final static int ThirdClassSeats=50;
+    private final static int FirstClassSeats=20;//
+    private final static int SecondClassSeats=40;//     The fixed seat for each class
+    private final static int ThirdClassSeats=50; //
 
-    private Character[][] FirstClass2dArr=new Character[4][5];
-    private Character[][] SecondClass2dArr=new Character[8][5];
-    private Character[][] ThirdClass2dArr=new Character[10][5];
+    private Character[][] FirstClass2dArr=new Character[4][5]; //
+    private Character[][] SecondClass2dArr=new Character[8][5]; //  The fixed size of the 2D arrays classes
+    private Character[][] ThirdClass2dArr=new Character[10][5]; //
 
     int FinalPayment=0,Row=0,Column=0,UserInputClass=0,UserInputID=0;
 
-    public Character[][] getFirstClass2dArr() {
-        return FirstClass2dArr;
-    }
-    public Character[][] getSecondClass2dArr() {
-        return SecondClass2dArr;
-    }
-    public Character[][] getThirdClass2dArr() {
-        return ThirdClass2dArr;
-    }
-    public void InitializeFirstClass2dArr() {
+
+    public void InitializeFirstClass2dArr() {   //  initialize all the class values with O
         for(int row=0;row<4;row++){
             for(int column=0;column<5;column++){
                 FirstClass2dArr[row][column]='O';
             }
         }
     }
-    public void InitializeSecondClass2dArr() {
+
+    public void InitializeSecondClass2dArr() {  //  initialize all the class values with O
         for(int row=0;row<8;row++){
             for(int column=0;column<5;column++){
                 SecondClass2dArr[row][column]='O';
             }
         }
     }
-    public void InitializeThirdClass2dArr() {
+
+    public void InitializeThirdClass2dArr() {  //  initialize all the class values with O
         for(int row=0;row<10;row++){
             for(int column=0;column<5;column++){
                 ThirdClass2dArr[row][column]='O';
             }
         }
     }
+
+
+
     // for testing
     public void print1(){
+        System.out.println();
         for(int row=0;row<4;row++){
             for(int column=0;column<5;column++){
                 System.out.print(FirstClass2dArr[row][column]+"\t");
@@ -74,7 +72,9 @@ public class theatre {
             System.out.println();
         }
     }
-    public void indexRowColumn(){
+
+
+    public void indexRowColumn(){   // the users enters the ID and it translate it into Row and Columns
         int storage=0, counts=-1;
         System.out.print("Enter the ID: ");
         UserInputID=input.nextInt();
@@ -91,23 +91,28 @@ public class theatre {
             Row=counts;
         }
     }
-    public void Reservation(){
+    public void TakeClassInput(){    // taking user class input
         System.out.print("Enter First Class: ");
         UserInputClass=input.nextInt();
+    }
+
+    public void Reservation(){
+        TakeClassInput();
         switch(UserInputClass){
             case 1:
                 try{
-                    indexRowColumn();
-                    if(FirstClass2dArr[Row][Column]=='O'){
-                        FirstClass2dArr[Row][Column]='X';
+                    indexRowColumn(); // gets the row column
+                    if(FirstClass2dArr[Row][Column]=='O'){   // check if its empty
+                        FirstClass2dArr[Row][Column]='X'; // fill it with X to make it resereved
                         FinalPayment+=FirstClassCost;
                     }
                     else{
-                        throw new Exception("Error: this seat is already taken!");
+                        throw new Exception("Error: this seat is already taken!"); // if the users enters ID already taken
                     }
                 }catch(Exception e){
                     System.out.print(e.getMessage());
                 }
+                print1();
                 break;
             case 2:
                 try{
@@ -117,12 +122,12 @@ public class theatre {
                         FinalPayment+=SecondClassCost;
                     }
                     else{
-                        throw new Exception("Error: this seat is already taken!");
+                        throw new Exception("Error: this seat is already taken!"); // if the users enters ID already taken
                     }
                 }catch(Exception e){
                     System.out.print(e.getMessage());
                 }
-
+                print2();
                 break;
             case 3:
                 try{
@@ -132,14 +137,94 @@ public class theatre {
                         FinalPayment+=ThirdClassCost;
                     }
                     else{
-                        throw new Exception("Error: this seat is already taken!");
+                        throw new Exception("Error: this seat is already taken!\n"); // if the users enters ID already taken
+
                     }
                 }catch(Exception e){
                     System.out.print(e.getMessage());
                 }
+                print3();
                 break;
         }
     }
 
+    public void CancelReservation() {
+        TakeClassInput();
+        switch (UserInputClass) {
+            /// Each index should be labelled with x or O ///
+            case 1:
+                try{
+                    indexRowColumn();
+                    if (FirstClass2dArr[Row][Column] == 'X') // Check if the seat is reserved
+                    {
+                        FirstClass2dArr[Row][Column] = 'O'; /// Now,Seat is empty
+                        FinalPayment -= FirstClassCost; // remove the seat price from total price
 
+                    }
+                    else {
+                        throw new Exception("Error: no reservation found at the specified seat."); // if the users enters ID already taken
+
+                    }
+                }catch(Exception e){
+                    System.out.print(e.getMessage());
+                }
+                print1();
+                break ;
+            case 2:
+                try{
+                    indexRowColumn();
+                    if (SecondClass2dArr[Row][Column] == 'X')
+                    {
+                            SecondClass2dArr[Row][Column] = 'O';
+                            FinalPayment -= SecondClassCost;
+                    } else {
+                        throw new Exception( "Error: no reservation found at the specified seat."); // if the users enters ID already taken
+
+                    }
+                }catch(Exception e){
+                    System.out.print(e.getMessage());
+                }
+                print2();
+                break ;
+            case 3:
+                try{
+                    indexRowColumn();
+                        if (ThirdClass2dArr[Row][Column] == 'X')
+                    {
+                        ThirdClass2dArr[Row][Column] = 'O';
+                        FinalPayment -= ThirdClassCost;
+
+
+                    } else {
+                        throw new Exception("Error: no reservation found at the specified seat."); // if the users enters ID already taken
+                    }
+                }catch(Exception e){
+                    System.out.print(e.getMessage());
+                }
+                print3();
+                break ;
+        }
+
+    }
+
+    public void ResetTheatreReservation(){ // it returns all the classes to its original form
+        InitializeFirstClass2dArr();
+        InitializeSecondClass2dArr();
+        InitializeThirdClass2dArr();
+        print1();
+        System.out.println("------------------------------------------------------");
+        print2();
+        System.out.println("------------------------------------------------------");
+        print3();
+        System.out.println("-----------------------------------------------------");
+        FinalPayment=0; // resets the payment too
+    }
+
+    ///// Function Exit ////
+    public void Exit(){
+        System.exit(1);  // parameter should be of  int type
+    }
 }
+
+
+
